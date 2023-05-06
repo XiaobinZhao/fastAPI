dafc91b18021c40e63822e2429cb0588ff7d5403
jia.wang1 <jia.wang1@cstack.io>
2023-05-06 11:41:27 +0800

    'modify'

--- myapp/models/__init__.py
+++ myapp/models/__init__.py
@@ -1,5 +1,6 @@
 from myapp.base.db import BaseModel
 from myapp.models.desktop import Desktop
 from myapp.models.user import User
+from myapp.models.jjtest import Jjtest
 
 metadata = BaseModel.metadata
